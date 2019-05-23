ce9341b653c3fe7b7a613888393d07f6f32f12e4
Uswah Saeed <saeedu@deakin.edu.au>
2019-05-23 13:27:00 +1000

    Final DialogFlow integration

--- src/components/dialogflow/intent.jsx
+++ src/components/dialogflow/intent.jsx
@@ -1,13 +1,28 @@
 import React, {Component} from 'react';
 import Axios from 'axios/index';
-import M from 'materialize-css';
-import LoadingComponent from '../loading/loading';
-import Register from '../register/register';
-import {Link} from 'react-router-dom';
+
 
 class Intent extends Component {
   constructor(props) {
     super(props)
+  }
+
+  getIntents(){
+    Axios.defaults.headers.common['Authorization'] = "Bearer ya29.c.EloSBze9DSxVnvZjAl9PwZRTmPitG87tDXSRnkR6NU7cyALnv5-KNMUATLUPpjwRWdiSWXXQAeDnEZb9dHsLQ2fYGmzIzvIMofbIQBh7tRnatQAtaWGxubAbslo";
+    Axios.get("https://dialogflow.googleapis.com/v2/projects/greetings-e8c46/agent/intents")
+      .then((result) => {
+        console.log("--------------->", result);
+        const intentResults = result.data.intents.map((res) =>
+          <tr><td>{res.displayName}</td></tr>
+        );
+        this.setState({
+          intentResult: intentResults
+        })
+      })
+    }
+
+componentDidMount() {
+  this.getIntents();
   }
 
   render() {
@@ -19,6 +34,9 @@ class Intent extends Component {
       <div className="" style={padding}>
         <span className="col s12 m12 l12">
           <h4 className="grey-text text-darken-3 lighten-3 left-align">Intent</h4>
+          <div className="collapsible-header grey-text text-darken-3 valign-wrapper">
+            <table>{(() => {if(this.state) {return this.state.intentResult}})()}</table>
+          </div>
         </span>
       </div>
     )
